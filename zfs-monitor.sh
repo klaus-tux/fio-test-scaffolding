843bd81e0272bd4b2b580709e7eb3560e72b3dc8
Klaus Sperner <klaus.sperner@gmail.com>
2023-02-10 08:35:20 +0100

    make watch iostat colorful

--- zfs-monitor.sh
+++ zfs-monitor.sh
@@ -1,1 +1,1 @@
-watch -n 1 'zpool iostat test -y -v 1 1 ; echo ; ls -l /dev/disk/by-id | grep WD120 | grep -v part | awk "{print \$11}" | sed "s#../../#/dev/#" | xargs iostat --human -xy 1 1'
+watch -c -n 1 'zpool iostat test -y -v 1 1 ; echo ; ls -l /dev/disk/by-id | grep WD120 | grep -v part | awk "{print \$11}" | sed "s#../../#/dev/#" | S_COLORS=always xargs iostat --human -xy 1 1'
