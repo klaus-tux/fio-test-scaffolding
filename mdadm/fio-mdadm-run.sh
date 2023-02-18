03897292430623812a9a0bb1d8ec603c4ea987ea
Klaus Sperner <klaus.sperner@gmail.com>
2023-02-18 13:06:16 +0100

    fix lazy inode table initialization parameter name

--- mdadm/fio-mdadm-run.sh
+++ mdadm/fio-mdadm-run.sh
@@ -7,7 +7,7 @@ echo "single disk, ext4"
 	mdadm --stop /dev/md1
 	DISK=`ls /dev/disk/by-id | grep WD120 | grep part1 | head -n 1 | sed 's#^#/dev/disk/by-id/#'`
 	wipefs -a $DISK
-	mkfs.ext4 -E lazy_table_init=0, lazy_journal_init=0 $DISK
+	mkfs.ext4 -E lazy_itable_init=0, lazy_journal_init=0 $DISK
 	mount $DISK /test
 	echo "Device /test is single-disk ext4 $DISK"
 	echo 
@@ -17,7 +17,7 @@ echo "mdraid10, internal bitmap, ext4"
 
 	# mdraid"10" 2 disks is just mdraid1 2 disks
 	./mdraidbuild.sh 1 2 internal
-	mkfs.ext4 -E lazy_table_init=0,lazy_journal_init=0 /dev/md1
+	mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/md1
 	mount /dev/md1 /test
 	./fio-mdadm-full-test.pl
 
@@ -25,7 +25,7 @@ echo "mdraid10, no bitmap, ext4"
 
 	# mdraid"10" 2 disks is just mdraid1 2 disks
 	./mdraidbuild.sh 1 2 none
-	mkfs.ext4 -E lazy_table_init=0,lazy_journal_init=0 /dev/md1
+	mkfs.ext4 -E lazy_itable_init=0,lazy_journal_init=0 /dev/md1
 	mount /dev/md1 /test
 	./fio-mdadm-full-test.pl
 
